29cd23c5c2bdce85239894ed0ac00ad615ac3bdd
Thenmozhi <stmolizhi17@gmail.com>
2020-08-12 14:18:20 +0530

    second commit

---
+++ simplemavenproject/src/main/java/com/honeyproject/Icalculator.java
@@ -0,0 +1,15 @@
+package com.honeyproject;
+
+public interface Icalculator {
+
+	int sum(int a, int b);
+
+	int subtraction(int a, int b);
+
+	int multiplication(int a, int b);
+
+	int divison(int a, int b) throws Exception;
+
+	boolean equalIntegers(int a, int b);
+
+}
